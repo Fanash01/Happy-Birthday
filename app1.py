ababeda0f3f05f9e04929bf52e2c3f1da86a7299
Fanash01 <113841196+Fanash01@users.noreply.github.com>
2023-04-19 15:51:11 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,7 +7,7 @@ def guess_number():
     st.write("Choose a number between 1 and 10:")
     number = st.number_input("", min_value=1, max_value=10, step=1, key="number_input")
     if st.button("Guess"):
-        if random.randint(1, 10) == number:
+        if random.randint(1, 3) == number:
             st.write("## 🎉 Congratulations! You guessed the correct number. 🎉")
             balloons_img = Image.open("balloons.jpg")
             st.image(balloons_img, use_column_width=True)
